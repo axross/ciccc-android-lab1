8ee34a83f183a7e6e6100ab7901de1e4f9b8e614
Kohei Asai <me@axross.io>
2019-04-17 11:46:56 -0700

    Rename the project

--- app/src/androidTest/java/app/axross/ciccc/lab1_renamed/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/app/axross/ciccc/lab1_renamed/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package app.axross.ciccc.lab1;
+package app.axross.ciccc.lab1_renamed;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("app.axross.ciccc.lab1", appContext.getPackageName());
+        assertEquals("app.axross.ciccc.lab1_renamed", appContext.getPackageName());
     }
 }
--- app/src/main/java/app/axross/ciccc/lab1_renamed/MainActivity.java
+++ app/src/main/java/app/axross/ciccc/lab1_renamed/MainActivity.java
@@ -1,4 +1,4 @@
-package app.axross.ciccc.lab1;
+package app.axross.ciccc.lab1_renamed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
